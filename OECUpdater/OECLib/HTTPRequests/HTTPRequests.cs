abdebc813f78b96d0292c2fd11c09f5292515378
Mike Petrella <thegluestickman@gmail.com>
2016-10-19 12:36:36 -0400

    edited requests name

---
+++ OECUpdater/OECLib/HTTPRequests/HTTPRequests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+
+namespace OECLib.HTTPRequests
+{
+	public class HTTPRequest
+	{
+		WebClient client;
+
+		public HTTPRequest () {
+			client = new WebClient ();
+		}
+
+		public string RequestAsString (string URL)
+		{
+			return client.DownloadString (URL);
+		}
+
+		public bool DownloadFile (string URL, string filename) {
+			try {
+				client.DownloadFile (URL, filename);
+				return true;
+			} catch (WebException e) {
+				return false;
+			}
+		}
+
+	}
+}
+
