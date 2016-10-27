a5bf4313e4bcb9c6b73e229577f1db36e0b82b12
Wannie Chen <Wannie923@gmail.com>
2016-10-26 22:38:36 -0400

    Built demo framework

--- OECUpdater/OECUpdater/PullRequestTest.cs
+++ OECUpdater/OECUpdater/PullRequestTest.cs
@@ -15,17 +15,30 @@ namespace OECUpdater
 {
     class PullRequestTest
     {
-        public static TcpListener listener;
         public static string localHost = "127.0.0.1";
         public static int port = 4567;
-        public static Session session;
+        public static string menu = "1)Check Access\n2)Display all pull-requests for repository\n3)Create Pull-Request for a repository\n4)Set Current Repository\n5)Logout";
+        public static Dictionary<String, Func<Task>> commands = new Dictionary<string, Func<Task>>();
+        public static Session s;
 
         public static void Main(string[] args)
         {
-                       
+            Console.WriteLine("Hello welcome to Spazio Demo!");
+            Console.WriteLine("Would you like to continue and login? Y/N");
+            initalizeCommands();
+            String resp = Console.ReadLine();
+            while (resp != "Y" && resp != "N")
+            {
+                Console.WriteLine("Enter either Y or N.");
+                resp = Console.ReadLine();
+            }
+            if (resp == "N")
+            {
+                return;
+            }
+
             GitHubClient g = new GitHubClient(new ProductHeaderValue("SpazioApp"));
             string csrf = Membership.GeneratePassword(24, 1);
-            //Session["CSRF:State"] = csrf;
 
             var request = new OauthLoginRequest(Session.clientId)
             {
@@ -37,24 +50,61 @@ namespace OECUpdater
             System.Diagnostics.Process.Start(oauthLoginUrl);
             
 
-            Authorize(g);
+            BeginGitHubSession(g);
 
+
+            while (1 == 1)
+            {
+
+            }
             
-            Console.ReadKey();
+        }
+
+        public static void initalizeCommands()
+        {
+            commands.Add("1", CheckAccess);
         }
 
 
-        public async static void Authorize(GitHubClient client)
+        public async static void BeginGitHubSession(GitHubClient client)
         {
             CallBackServer server = new CallBackServer(localHost, port, client);
-            Session s = await server.Start();
+            s = await server.Start();
             Console.WriteLine(s.ToString());
-            PullRequest(s);
+            Console.WriteLine("GitHub session started...");
+
+            while (1 == 1)
+            {
+                Console.WriteLine("Type a command!");
+                Console.WriteLine(menu);
+                Console.WriteLine("Command: ");
+                String cmd = Console.ReadLine();
+                Console.WriteLine(cmd);
+                while (!commands.ContainsKey(cmd))
+                {
+                    Console.WriteLine("Type a correct command...");
+                    Console.WriteLine(menu);
+                    Console.WriteLine("Command: ");
+                    cmd = Console.ReadLine();
+                }
+                await commands[cmd]();
+            }
 
         }
 
-        public async static void PullRequest(Session s) {
-            bool hasAccess = await CheckAccessAsync(s);
+        public async static Task CheckAccess()
+        {
+            Console.WriteLine("Repository Owner: ");
+            String owner = Console.ReadLine();
+            Console.WriteLine("Repository Name: ");
+            String name = Console.ReadLine();
+
+            bool hasAccess = await CheckAccessAsync(owner, name);
+            Console.WriteLine(hasAccess ? "You are a contributer of this repo!" : "You don't have access to this repo and cannot use this application!");
+        }
+
+        public async static void PullRequest() {
+            bool hasAccess = await CheckAccessAsync("Gazing", "RedditPostsSaver");
             Console.WriteLine(hasAccess ? "You are a contributer of this repo!" : "You don't have access to this repo and cannot use this application!");
             if (!hasAccess)
             {
@@ -73,10 +123,20 @@ namespace OECUpdater
             
         }
 
-        public async static Task<bool> CheckAccessAsync(Session s)
+        public async static void AllPullRequests()
+        {
+            var repo = await s.client.Repository.Get("Gazing", "RedditPostsSaver");
+            var prs = await s.client.PullRequest.GetAllForRepository(repo.Id, new ApiOptions());
+            foreach (PullRequest pr in prs)
+            {
+                Console.WriteLine(pr.Title);
+            }
+        }
+
+        public async static Task<bool> CheckAccessAsync(String owner, String name)
         {
             ApiOptions options = new ApiOptions();
-            var repos = await s.client.Repository.GetAllContributors("Gazing", "RedditPostsSaver");
+            var repos = await s.client.Repository.GetAllContributors(owner, name);
             User cur = await s.client.User.Current();
             int id = cur.Id;
             foreach (RepositoryContributor person in repos)
