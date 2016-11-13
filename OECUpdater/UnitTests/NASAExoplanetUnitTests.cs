5942561f1c6f1d8c7d895d25db9da523588b3258
Michael Liu <mikeliu_2011@hotmail.com>
2016-11-13 12:05:47 -0500

    Added UnitTests for ExoplanetEU and NASAExoplanet

---
+++ OECUpdater/UnitTests/NASAExoplanetUnitTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NASAExoplanetPlugin;
+using OECLib.Data;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+	public class NASAExoplanetUnitTests
+	{
+		NASAExoplanet nasaexoplanet = new NASAExoplanet();
+
+		public void GetNameTest()
+		{
+			string value = nasaexoplanet.GetName();
+			Assert.AreEqual("Exoplanet.NASA", value);
+		}
+
+		public void GetDescriptionTest()
+		{
+			string value = nasaexoplanet.GetDescription();
+			Assert.AreEqual("This plugin allows the extraction of data from the NASA Exoplanet Archive", value);
+		}
+
+		public void GetAuthorTest()
+		{
+			string value = nasaexoplanet.GetAuthor();
+			Assert.AreEqual("Spazio", value);
+		}
+
+		public void RunTest()
+		{
+			//Dictionary<string, Star> stars = new Dictionary<string, Star>();
+			//List<Star> starlist = new List<Star>();
+			//starlist.AddRange(stars.Values);
+
+			List<Star> starlist = nasaexoplanet.Run();
+			List<Star> expectedlist = nasaexoplanet.Run();
+
+			for (int i = 0; i < starlist.Count; i = i + 1)
+			{
+				Assert.AreEqual(expectedlist[i], starlist[i]);
+			}
+		}
+	}
+}
