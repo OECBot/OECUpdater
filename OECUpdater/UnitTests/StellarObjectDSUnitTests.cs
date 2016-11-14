b7882829ff9dabe3aad6288239978da5b900fb89
Mark Anthony Viola <mark.anthony.viola@gmail.com>
2016-11-13 20:37:28 -0500

    Added new unit tests for Star

--- OECUpdater/UnitTests/StellarObjectDSUnitTests.cs
+++ OECUpdater/UnitTests/StellarObjectDSUnitTests.cs
@@ -14,11 +14,10 @@ namespace UnitTests
     [TestFixture]
     class StellarObjectDSUnitTests
     {
-        private SolarSystem solarSystem;
-        private Binary binary;
-        private Star star;
-        private Planet planet;
-        //private XmlDocument doc;
+        //private SolarSystem solarSystem;
+        //private Binary binary;
+        //private Star star;
+        //private Planet planet;
 
         private NumberErrorMeasurement numErrMeasurement;
         private NumberMeasurement numMeasurement;
@@ -28,34 +27,37 @@ namespace UnitTests
         [SetUp]
         protected void SetUp()
         {
-            solarSystem = new SolarSystem();
-            binary = new Binary();
-            star = new Star();
-            planet = new Planet();
-            //doc = new XmlDocument();
+            //solarSystem = new SolarSystem();
+            //binary = new Binary();
+            //star = new Star();
+            //planet = new Planet();
 
             numErrMeasurement = new NumberErrorMeasurement("magB", 5.74, 0.02, 0.03);
             numMeasurement = new NumberMeasurement("magJ", 2.943);
             strMeasurement = new StringMeasurement("spectraltype", "G8 III");
         }
 
+        // Unit tests for Planet
+
         [Test]
         public void PlanetAddChild()
         {
+            Planet currentPlanet = new Planet();
+
             SolarSystem tempSolarSystem = new SolarSystem();
-            bool addSolarSystem = planet.AddChild(tempSolarSystem);
+            bool addSolarSystem = currentPlanet.AddChild(tempSolarSystem);
             Assert.AreEqual(false, addSolarSystem);
 
             Binary tempBinary = new Binary();
-            bool addBinary = planet.AddChild(tempBinary);
+            bool addBinary = currentPlanet.AddChild(tempBinary);
             Assert.AreEqual(false, addBinary);
 
             Star tempStar = new Star();
-            bool addStar = planet.AddChild(tempStar);
+            bool addStar = currentPlanet.AddChild(tempStar);
             Assert.AreEqual(false, addStar);
 
             Planet tempPlanet = new Planet();
-            bool addPlanet = planet.AddChild(tempPlanet);
+            bool addPlanet = currentPlanet.AddChild(tempPlanet);
             Assert.AreEqual(false, addPlanet);
         }
 
@@ -63,10 +65,10 @@ namespace UnitTests
         public void CheckPlanetType()
         {
             Planet tempPlanet = new Planet();
-            Assert.AreEqual(false, planet.IsASystem);
-            Assert.AreEqual(false, planet.IsABinary);
-            Assert.AreEqual(false, planet.IsAStar);
-            Assert.AreEqual(true, planet.IsAPlanet);
+            Assert.AreEqual(false, tempPlanet.IsASystem);
+            Assert.AreEqual(false, tempPlanet.IsABinary);
+            Assert.AreEqual(false, tempPlanet.IsAStar);
+            Assert.AreEqual(true, tempPlanet.IsAPlanet);
         }
 
         [TestCase("spectraltype", "G8 III")]
@@ -118,7 +120,7 @@ namespace UnitTests
         }
 
         [Test]
-        public void CheckPlanetAddMeasurementsTag()
+        public void CheckPlanetXMLTag()
         {
             Planet tempPlanet = new Planet();
             XmlDocument doc = new XmlDocument();
@@ -126,6 +128,114 @@ namespace UnitTests
             tempPlanet.AddMeasurement(numMeasurement);
             tempPlanet.AddMeasurement(strMeasurement);
             XmlElement element = tempPlanet.XMLTag(doc);
+
+            Assert.AreEqual("planet", element.Name);
+
+            Assert.AreEqual("magB", element.ChildNodes[0].Name);
+            Assert.AreEqual("5.74", element.ChildNodes[0].InnerText);
+            Assert.AreEqual("0.02", element.ChildNodes[0].Attributes[0].InnerText);
+            Assert.AreEqual("0.03", element.ChildNodes[0].Attributes[1].InnerText);
+
+            Assert.AreEqual("magJ", element.ChildNodes[1].Name);
+            Assert.AreEqual("2.943", element.ChildNodes[1].InnerText);
+
+            Assert.AreEqual("spectraltype", element.ChildNodes[2].Name);
+            Assert.AreEqual("G8 III", element.ChildNodes[2].InnerText);
+        }
+
+        // Unit tests for Star
+
+        [Test]
+        public void StarAddChild()
+        {
+            Star currentStar = new Star();
+
+            SolarSystem tempSolarSystem = new SolarSystem();
+            bool addSolarSystem = currentStar.AddChild(tempSolarSystem);
+            Assert.AreEqual(false, addSolarSystem);
+
+            Binary tempBinary = new Binary();
+            bool addBinary = currentStar.AddChild(tempBinary);
+            Assert.AreEqual(false, addBinary);
+
+            Star tempStar = new Star();
+            bool addStar = currentStar.AddChild(tempStar);
+            Assert.AreEqual(false, addStar);
+
+            Planet tempPlanet = new Planet();
+            bool addPlanet = currentStar.AddChild(tempPlanet);
+            Assert.AreEqual(true, addPlanet);
+        }
+
+        [Test]
+        public void CheckStarType()
+        {
+            Star tempStar = new Star();
+            Assert.AreEqual(false, tempStar.IsASystem);
+            Assert.AreEqual(false, tempStar.IsABinary);
+            Assert.AreEqual(true, tempStar.IsAStar);
+            Assert.AreEqual(false, tempStar.IsAPlanet);
+        }
+
+        [TestCase("spectraltype", "G8 III")]
+        public void CheckStarAddStringMeasurement(string name, string measurement)
+        {
+            Star tempStar = new Star();
+            tempStar.AddStringMeasurement(name, measurement);
+            Assert.AreEqual(true, tempStar.measurements.ContainsKey(name));
+            Assert.AreEqual(measurement, (string)tempStar.measurements[name].getValue().value);
+        }
+
+        [TestCase("magB", 5.74)]
+        public void CheckStarAddNumberMeasurement(string name, double measurement)
+        {
+            Star tempStar = new Star();
+            tempStar.AddNumberMeasurement(name, measurement);
+            Assert.AreEqual(true, tempStar.measurements.ContainsKey(name));
+            Assert.AreEqual(measurement, (double)tempStar.measurements[name].getValue().value);
+        }
+
+        [TestCase("magB", 5.74, 0.02, 0.02)]
+        public void CheckStarAddNumberErrorMeasurement(string name, double measurement, double errPlus, double errMinus)
+        {
+            Star tempStar = new Star();
+            tempStar.AddNumberErrorMeasurement(name, measurement, errPlus, errMinus);
+            Assert.AreEqual(true, tempStar.measurements.ContainsKey(name));
+            Assert.AreEqual(measurement, (double)tempStar.measurements[name].getValue().value);
+            Assert.AreEqual(errPlus, tempStar.measurements[name].getValue().errorPlus);
+            Assert.AreEqual(errMinus, tempStar.measurements[name].getValue().errorMinus);
+        }
+
+        [Test]
+        public void CheckStarAddMeasurement()
+        {
+            Star tempStar = new Star();
+            tempStar.AddMeasurement(numErrMeasurement);
+            Assert.AreEqual(true, tempStar.measurements.ContainsKey("magB"));
+            Assert.AreEqual(5.74, (double)tempStar.measurements["magB"].getValue().value);
+            Assert.AreEqual(0.02, (double)tempStar.measurements["magB"].getValue().errorPlus);
+            Assert.AreEqual(0.03, (double)tempStar.measurements["magB"].getValue().errorMinus);
+
+            tempStar.AddMeasurement(numMeasurement);
+            Assert.AreEqual(true, tempStar.measurements.ContainsKey("magJ"));
+            Assert.AreEqual(2.943, (double)tempStar.measurements["magJ"].getValue().value);
+
+            tempStar.AddMeasurement(strMeasurement);
+            Assert.AreEqual(true, tempStar.measurements.ContainsKey("spectraltype"));
+            Assert.AreEqual("G8 III", (string)tempStar.measurements["spectraltype"].getValue().value);
+        }
+
+        [Test]
+        public void CheckStarXMLTag()
+        {
+            Star tempStar = new Star();
+            XmlDocument doc = new XmlDocument();
+            tempStar.AddMeasurement(numErrMeasurement);
+            tempStar.AddMeasurement(numMeasurement);
+            tempStar.AddMeasurement(strMeasurement);
+            XmlElement element = tempStar.XMLTag(doc);
+
+            Assert.AreEqual("star", element.Name);
 
             Assert.AreEqual("magB", element.ChildNodes[0].Name);
             Assert.AreEqual("5.74", element.ChildNodes[0].InnerText);
