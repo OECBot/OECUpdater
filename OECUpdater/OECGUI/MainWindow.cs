df2cc48fb3c6cdf28db728598429c8861b5bb633
Gazing <wannie923@gmail.com>
2016-11-24 20:26:24 -0500

    New design

--- OECUpdater/OECGUI/MainWindow.cs
+++ OECUpdater/OECGUI/MainWindow.cs
@@ -5,12 +5,14 @@ using OECLib.Utilities;
 using UI = Gtk.Builder.ObjectAttribute;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace OECGUI
 {
 	public class MainWindow: Gtk.Window
 	{
 		[UI] Notebook notebook1;
+		[UI] Image image1;
 		BotForm bf;
 
 		public static MainWindow Create (RepositoryManager manager)
@@ -47,16 +49,59 @@ namespace OECGUI
 
 
 			bf = BotForm.Create (manager);
-			//var box = new HBox ();
-			//var icon = new Image ("Assets/home.png");
+
+			var box3 = new HBox ();
+
+			var pixbuffloader = new Gdk.PixbufLoader ("Assets/push.png", 32, 32);
+			var pixbuff = pixbuffloader.Pixbuf;
+			//pixbuff.ScaleSimple (36, 36, Gdk.InterpType.Hyper);
+			var icon2 = new Image (pixbuff);
+			
+			Label l3 = new Label ("Pull-Requests");
+			box3.PackStart (icon2, false, false, 0);
+			l3.StyleContext.AddClass ("fontWhite");
+			box3.PackStart (l3, false, false, 0);
+
+			var box4 = new HBox ();
+			pixbuffloader = new Gdk.PixbufLoader ("Assets/plugin.png", 32, 32);
+			var icon3 = new Image (pixbuffloader.Pixbuf);
+			box4.PackStart (icon3, false, false, 0);
+			var l4 = new Label ("Plugins");
+			l4.StyleContext.AddClass ("fontWhite");
+			box4.PackStart (l4, false, false, 0);
+			box4.ShowAll ();
+
+			var box = new HBox ();
+			pixbuffloader = new Gdk.PixbufLoader ("Assets/home.png", 32, 32);
+			var img = new Image (pixbuffloader.Pixbuf);
+
+
+			var box2 = new HBox();
+			pixbuffloader = new Gdk.PixbufLoader ("Assets/power.png", 32, 32);
+			var icon = new Image (pixbuffloader.Pixbuf);
+
+			box2.PackStart (icon, false, false, 0);
+
+			box.PackStart (img, false, false, 0);
 			Label l1 = new Label ("OECBot");
 			l1.StyleContext.AddClass ("fontWhite");
-
+			box2.PackStart (l1, false, false, 0);
 			Label l2 = new Label ("Dashboard");
 
+			box.PackStart (l2, false, false, 0);
 			l2.StyleContext.AddClass ("fontWhite");
-			notebook1.AppendPage (DashboardForm.Create(), l2);
-			notebook1.AppendPage (bf, l1);
+
+			pixbuffloader = new Gdk.PixbufLoader ("Assets/logo.png", 110, 40);
+			image1.Pixbuf = pixbuffloader.Pixbuf;
+
+			box.ShowAll ();
+			box2.ShowAll ();
+			box3.ShowAll ();
+
+			notebook1.AppendPage (DashboardForm.Create(), box);
+			notebook1.AppendPage (bf, box2);
+			notebook1.AppendPage (DashboardForm.Create (), box4);
+			notebook1.AppendPage (DashboardForm.Create (), box3);
 
 
 
@@ -64,6 +109,8 @@ namespace OECGUI
 			provider.LoadFromPath ("test.css");
 			l1.StyleContext.AddProvider (provider, uint.MaxValue);
 			l2.StyleContext.AddProvider (provider, uint.MaxValue);
+			l3.StyleContext.AddProvider (provider, uint.MaxValue);
+			l4.StyleContext.AddProvider (provider, uint.MaxValue);
 			ApplyCss (this, provider, uint.MaxValue);
 			//ShowAll ();
 		}
