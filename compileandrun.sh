60f86e0f7acd8eabfdaa401a36a234922c5be458
Mike Petrella <thegluestickman@gmail.com>
2016-12-09 11:17:46 -0500

    Update compileandrun.sh
    compile and run script moves plugins

--- compileandrun.sh
+++ compileandrun.sh
@@ -2,7 +2,14 @@
 
 xbuild /p:Configuration=Release OECUpdater/OECUpdater.sln
 
+rm -rf Release
+
 mv OECUpdater/OECGUI/bin/Release Release
 
 cd Release
+mkdir -p Plugins
+
+mv NASAExoplanetPlugin.dll Plugins/NASAExoplanetPlugin.dll
+mv ExoplanetEU.dll Plugins/ExoplanetEU.dll
+
 mono OECGUI.exe
